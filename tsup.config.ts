4b5938d85a50bc3fa1b38821c9c5b8921c7dfc5e
Dionisio Vieira <dionisioviei@gmail.com>
2024-01-04 10:17:38 -0300

    change esbuild to tsup

---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  define: {
+    "process.env.NODE_ENV": '"production"',
+  },
+  minify: true,
+  outDir: "dist",
+  target: "node18",
+  dts: true,
+});
